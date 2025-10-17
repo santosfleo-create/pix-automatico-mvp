8096e8ba8748b77c4746effd7dd39639ca8ae328
santosfleo-create <santosf.leo@gmail.com>
2025-10-17 22:47:02 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173
+  }
 })
